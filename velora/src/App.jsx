e2bcbbba505400781a16ad7657fb3587dd361f28
Aurel <aweaurel229@gmail.com>
2026-06-21 20:22:51 +0700

    membuat perpindahan page make route

--- velora/src/App.jsx
+++ velora/src/App.jsx
@@ -1,13 +1,25 @@
-import Header from "./component/Header"
-import Beranda from "./pages/Beranda";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
 import Footer from "./component/footer";
+import Beranda from "./pages/Beranda";
+import Produk from "./pages/Produk";
+import Keranjang from "./pages/Keranjang";
+
 const App = () => {
-  return(
-    <>
-    <Header/>
-    <Beranda />
-    <Footer />
-    </>
+  return (
+    <Router>
+      <Header />
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Beranda />} />
+          <Route path="/produk" element={<Produk />} />
+          <Route path="/keranjang" element={<Keranjang />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
+
 export default App;
